90ace54b7dd8d1ea49db1f67f5c567f4f2acc7b3
Thijs Boom <twboom@hotmail.nl>
2020-12-18 23:54:34 +0100

    Made it kinda working

--- app/js/playMusic.js
+++ app/js/playMusic.js
@@ -1,5 +1,8 @@
-function playDemo() {
-    var audio = new Audio('../app/assets/theLick.wav');
+var songList = require('./assets/songs.json')
+var song = songs[Math.floor(Math.random() * items.length)];
+    console.log(song)
+
+function play() {
+    var audio = new Audio(song.path);
     audio.play();
 };
-
