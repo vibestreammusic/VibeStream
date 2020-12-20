1f6e3792e237842f4a68e0e859ccf8984a88d31a
Thijs Boom <twboom@hotmail.nl>
2020-12-21 00:47:05 +0100

    Everything kinda works now

--- app/js/helper/readJSON.js
+++
@@ -1,8 +0,0 @@
-var readJson = (path, cb) => {
-    fs.readFile(require.resolve(path), (err, data) => {
-      if (err)
-        cb(err)
-      else
-        cb(null, JSON.parse(data))
-    })
-  }
--- app/js/playMusic.js
+++ app/js/playMusic.js
@@ -1,8 +1,13 @@
-var songList = require('./assets/songs.json')
-var song = songs[Math.floor(Math.random() * items.length)];
-    console.log(song)
-
 function play() {
+    var song = songList[Math.floor(Math.random() * songList.length)];
+    console.log(song);
     var audio = new Audio(song.path);
     audio.play();
 };
+
+fetch('https://assets-vibestream.netlify.app/assets/data/songList.json').then((r) => {
+    return r.json()
+  }).then((j) => {
+    console.log(j)
+    x()
+  })
