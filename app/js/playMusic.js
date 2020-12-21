466598f66ac185aed6066960fcbe8dd12fe4e721
Thijs Boom <twboom@hotmail.nl>
2020-12-21 01:34:13 +0100

    Made it almost work

--- app/js/playMusic.js
+++ app/js/playMusic.js
@@ -1,3 +1,5 @@
+var songList
+
 function play() {
     var song = songList[Math.floor(Math.random() * songList.length)];
     console.log(song);
@@ -7,7 +9,7 @@ function play() {
 
 fetch('https://assets-vibestream.netlify.app/assets/data/songList.json').then((r) => {
     return r.json()
-  }).then((j) => {
-    console.log(j)
-    x()
+  }).then((songList) => {
+    console.log(songList)
+    play()
   })
