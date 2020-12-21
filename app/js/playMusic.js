60ca6c6c90984695ed5bc1e2eacee0a348e05fd6
Thijs Boom <twboom@hotmail.nl>
2020-12-21 14:29:33 +0100

    The basics are working

--- app/js/playMusic.js
+++ app/js/playMusic.js
@@ -1,15 +1,27 @@
-var songList
-
-function play() {
+function play(songList) {
     var song = songList[Math.floor(Math.random() * songList.length)];
     console.log(song);
     var audio = new Audio(song.path);
     audio.play();
+
+    document.querySelector('#songName').innerHTML = song.name
 };
 
-fetch('https://assets-vibestream.netlify.app/assets/data/songList.json').then((r) => {
-    return r.json()
-  }).then((songList) => {
-    console.log(songList)
-    play()
-  })
+
+document.getElementById('playbutton').addEventListener('click', _ => {
+    fetch('https://assets-vibestream.netlify.app/assets/data/songList.json').then((r) => {
+        return r.json()
+    }).then(play)
+})
+
+
+
+
+/*
+window.myappname = {}
+
+
+
+//
+window.myappname.songLIst = input
+*/
