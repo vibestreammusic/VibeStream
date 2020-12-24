f795f877e4a05fbcc8b04dc26bf0373f499acbbe
Thijs Boom <twboom@hotmail.nl>
2020-12-24 02:22:43 +0100

    Still aint workin

--- app/js/playMusic.js
+++ app/js/playMusic.js
@@ -1,18 +1,36 @@
+const url = 'https://assets-vibestream.netlify.app/assets/data/songList.json';
+var songList;
+
+fetch(url).then(
+    function(u){ return u.json();}
+  ).then(
+    function(json){
+      songList = json;
+    }
+  )
+
 function play(songList) {
     var song = songList[Math.floor(Math.random() * songList.length)];
     console.log(song);
     var audio = new Audio(song.path);
     audio.play();
 
-    document.querySelector('#songName').innerHTML = song.name
+    document.querySelector('#songName').innerHTML = song.name;
 };
 
+document.querySelector('#playbutton').addEventListener('click', play(songList));
 
+
+
+
+/*
 document.getElementById('playbutton').addEventListener('click', _ => {
     fetch('https://assets-vibestream.netlify.app/assets/data/songList.json').then((r) => {
         return r.json()
     }).then(play)
 })
+    */
+
 
 
 
